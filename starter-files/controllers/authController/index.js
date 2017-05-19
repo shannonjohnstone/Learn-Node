bb3a321163b1dca9fcaf410e6494f0139d98eca3
shannonjohnstone <shannon@shannonjohnstone.com.au>
2017-05-19 17:06:21 +1000

    updating authController, User model and login forms

--- starter-files/controllers/authController/index.js
+++ starter-files/controllers/authController/index.js
@@ -1,4 +1,8 @@
+const mongoose = require('mongoose')
+const user = mongoose.model('User')
 const passport = require('passport')
+const crypto = require('crypto')
+
 
 // any strageries used must be setup
 exports.login = passport.authenticate('local', {
@@ -19,4 +23,19 @@ exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) return next()
   req.flash('error', 'Oops you must be logged in!')
   res.redirect('login')
+}
+
+/* forgot - used for password forget functionality
+ * find user, if user found set reset token and token expire token */
+exports.forgot = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) {
+    req.flash('error', 'No account with that email exists')
+    res.redirect('/login')
+  }
+
+  // if there is a user set the tokens
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex')
+  user.resetPasswordExpires = Date.now() + 3600000
+  await user.save()
 }
