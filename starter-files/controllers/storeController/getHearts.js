cb197cef656d1f35b28df48818805863bd7d74e4
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2017-05-23 08:51:37 +1000

    update

--- starter-files/controllers/storeController/getHearts.js
+++ starter-files/controllers/storeController/getHearts.js
@@ -7,3 +7,4 @@ module.exports = async (req, res) => {
   const stores = await Store.find({ _id: { $in: req.user.hearts } }) // method 2: find stores that match the ids in the users hearts array, this way does not expose the user details
   res.render('stores', { title: 'Hearted stores', stores })
 }
+ 
